8ee64d082d9571e3fbc2bf61605a1cd4833fc259
dhenmasadhi <dhenmasa@gmail.com>
2024-03-04 08:17:01 +0700

    menambahkan modal project teams

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const Hidden = document.getElementById("navigation");
 menuIcon.addEventListener("click", () => {
   Hidden.classList.toggle("navigation");
 });
+
+function toggleModal1() {
+  document.getElementById("modal-1").classList.toggle("active");
+}
+
+function toggleModal2() {
+  document.getElementById("modal-2").classList.toggle("active");
+}
